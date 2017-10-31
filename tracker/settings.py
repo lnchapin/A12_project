764659ba0924cbafe38642bfa822e97fa2efeb38
Lindsay Chapin <lnchapin@gmail.com>
2017-10-31 11:44:50 -0600

    cors fix

--- tracker/settings.py
+++ tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!mx%&jb3xain)1j$08v8^7fr)7qo#_f$vv-gvuv_l1%-#!%g*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'grim-corpse-27385.herokuapp.com']
 
 
 # Application definition
