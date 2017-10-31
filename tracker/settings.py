5cf21e0fd6bedcf65e353ccffd531f05ecf76e8f
Lindsay Chapin <lnchapin@gmail.com>
2017-10-31 11:09:58 -0600

    heroku

--- tracker/settings.py
+++ tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!mx%&jb3xain)1j$08v8^7fr)7qo#_f$vv-gvuv_l1%-#!%g*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
